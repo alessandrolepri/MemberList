d4792b2a3f396b86e1bf15d7e2c32308b5df94f3
alepri <alessandro.lepri@selfridges.co.uk>
2020-03-17 10:43:23 +0000

    Added Member List into object

--- src/app.js
+++ src/app.js
@@ -1,1 +1,45 @@
 import './scss/style.scss'
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    function CreateTable() {
+        var members = [{
+
+                "Team Member": "Erica Badu",
+                "Email": "e.badu@example.com",
+                "Permission Level": "Owner"
+            },
+            {
+
+                "Team Member": "Pat Nelsson",
+                "Email": "p.nelsson@example.com",
+                "Permission Level": "Admin"
+            },
+            {
+
+                "Team Member": "Pending acceptance",
+                "Email": "j.dog@example.com",
+                "Permission Level": "Standard"
+            },
+            {
+
+                "Team Member": "Amy Namy",
+                "Email": "a.namy@example.com",
+                "Permission Level": "Standard"
+            },
+            {
+
+                "Team Member": "Victor D.",
+                "Email": "v.d@example.com",
+                "Permission Level": "Standard"
+            },
+            {
+
+                "Team Member": "Olly",
+                "Email": "o.hunter@example.com",
+                "Permission Level": "Standard"
+            },
+        ]
+    }
+})
